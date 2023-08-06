ff3f5a1b391d180fae9fcc6bc17ea34bebc4285b
tanjiro045 <136693903+tanjiro045@users.noreply.github.com>
2023-08-06 22:18:21 +0530

    Add files via upload

---
+++ prac9/codes/prac9b/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Practical_9b.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical_9b
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Service1Client service1Client = new Service1Client();
+            DataSet ds = service1Client.getEmployees();
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+    }
+}
