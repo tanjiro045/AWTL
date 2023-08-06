d07c547496d011708e44d8a350dce19192c97be1
tanjiro045 <136693903+tanjiro045@users.noreply.github.com>
2023-08-06 21:04:39 +0530

    Practical 7a - linq

---
+++ prac7/codes/Practical_7a_GridView/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practical7New.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             ID:
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="display" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="show all records" />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ prac7/codes/Practical_7a_GridView/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical7New
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(TextBox1.Text);
+            DataClasses1DataContext dbCon = new DataClasses1DataContext("Data Source=LAPTOP-LABV26J8;Initial Catalog=MCA;Integrated Security=True");
+            GridView1.DataSource = (from stud in dbCon.STUDENTs where stud.ID == id select stud);
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dbCon = new DataClasses1DataContext("Data Source=LAPTOP-LABV26J8;Initial Catalog=MCA;Integrated Security=True");
+            GridView1.DataSource = (from row in dbCon.STUDENTs select row);
+            GridView1.DataBind();
+        }
+    }
+}
