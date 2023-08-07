6678206a9c3df9b0b64101ee41866733c777f773
tanjiro045 <136693903+tanjiro045@users.noreply.github.com>
2023-08-07 12:52:54 +0530

    Add files via upload

---
+++ prac10/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practical10A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ prac10/Model.cs
@@ -0,0 +1,11 @@
+namespace Practical10A.Models
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Subject { get; set; }
+
+        public string? Degree { get; set; }
+    }
+}
